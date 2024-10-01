e87fe4288f02f17add47bf89122c04d6dec16cf1
Anir EL KABIRI <anir@sijo.fr>
2024-10-01 16:29:28 +0200

    api

---
+++ src/backend/api.js
@@ -0,0 +1,22 @@
+// api.js
+import { insert } from 'wix-data';
+
+export async function post_data(request) {
+    let options = {
+        "suppressAuth": true,
+        "suppressHooks": true
+    };
+
+    const body = await request.body.json();
+
+    const result = await insert("Classement", {
+        prenom: body.prenom,
+        Score: body.Score,
+        Equipe: body.Equipe
+    }, options);
+
+    return {
+        status: 200,
+        body: { message: 'Data added successfully', result }
+    };
+}
